a8ad223654dbbeab5ce02db1e5890049c6660528
Daniel Neel <dneel10@gmail.com>
2025-01-10 17:38:59 -0500

    set up basic application controller to initialize app. installed date-fns package

---
+++ src/appController.js
@@ -0,0 +1,24 @@
+import Project from "./project.js";
+import Todo from "./todo.js";
+import { format, startOfToday } from "date-fns";
+
+export function initializeApp() {
+  const date = format(startOfToday(), "MM/dd/yyyy");
+  console.log(date);
+  const projects = []; // initialize an empty array to manage multiple projects
+
+  // create default projects and todos to start with
+  const defaultProject = Project("Your Project", `${date}`);
+  const defaultTodo = Todo(
+    "Feed the Dog",
+    "Make sure to feed the dog 1 cup of food in the morning",
+    `${date}`
+  );
+
+  defaultProject.addTodo(defaultTodo);
+
+  projects.push(defaultProject);
+
+  // return the initialized app state
+  return { projects, currentProject: defaultProject };
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import createTodo from "./todo.js";
-import createProject from "./project.js";
+import { initializeApp } from "./appController";
 
-const todo = createTodo("test", "testing", "1-8-2025", "high");
-const project = createProject("testProj", "1-9-2025");
+const app = initializeApp();
 
-console.log(todo);
-console.log(project);
-
-project.addTodo(todo);
-
-window.createTodo = createTodo;
-window.createProject = createProject;
-
-window.todo = todo;
-window.project = project;
+window.app = app;
